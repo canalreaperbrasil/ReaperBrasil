7da7733d5f3175a2f4e9a5dc6064c94c19c98378
canalreaperbrasil <canalreaperbrasil@gmail.com>
2026-05-27 11:32:45 -0300

    Update stereo para 2 monos.lua

--- Scripts/stereo para 2 monos.lua
+++ Scripts/stereo para 2 monos.lua
@@ -1,3 +1,13 @@
+local cmd = reaper.NamedCommandLookup("_XENAKIOS_PANTRACKSLEFT")
+
+if cmd == 0 then
+    reaper.MB(
+        "Esta script requer a extensão SWS.\n\nInstale-a em:\nhttps://sws-extension.org/",
+        "Atenção",
+        0
+    )
+    return
+end
 reaper.NamedCommandLookup("XENAKIOS_PANTRACKSLEFT")
 reaper.Main_OnCommand(40421, 0)
 reaper.Main_OnCommand(40179, 0)
