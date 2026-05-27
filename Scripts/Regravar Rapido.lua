b490176c46d6743f6a545c801c334fbc9d940d2f
canalreaperbrasil <canalreaperbrasil@gmail.com>
2026-05-27 12:08:03 -0300

    Update Regravar Rapido.lua

--- Scripts/Regravar Rapido.lua
+++ Scripts/Regravar Rapido.lua
@@ -1,3 +1,7 @@
-reaper.Main_OnCommand(40044, 0)
+-- @description Regravar Rápido.lua
+-- @author Reaper Brasil
+-- @version 1.1
+-- @about Regrava rapidamente se você está gravando e erra
+
 reaper.Main_OnCommand(40029, 0)
 reaper.Main_OnCommand(1013, 0)
