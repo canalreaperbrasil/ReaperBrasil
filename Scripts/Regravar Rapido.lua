41787aff3ba7f609a43eea24d3fdf21b2354054b
canalreaperbrasil <canalreaperbrasil@gmail.com>
2026-05-28 16:59:50 -0300

    Update Regravar Rapido.lua

--- Scripts/Regravar Rapido.lua
+++ Scripts/Regravar Rapido.lua
@@ -1,2 +1,4 @@
-reaper.Main_OnCommand(40029, 0)
-reaper.Main_OnCommand(1013, 0)
+-- Executa as três ações sequencialmente
+reaper.Main_OnCommand(40044, 0) -- Transport: Play/stop
+reaper.Main_OnCommand(40029, 0) -- Edit: Undo
+reaper.Main_OnCommand(1013, 0)  -- Transport: Record
